a28fa43e47c4885c2eb69e7f486bc70aec1e7410
Sahidul Islam <sahidul@technoexponent.com>
2019-09-19 11:38:40 +0530

    Initial Commit

---
+++ TestAppUseMVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestAppUseMVVM
+//
+//  Created by Technoexponent on 19/09/19.
+//  Copyright © 2019 Technoexponent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
